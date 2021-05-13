497895b5e0276f16bd3a3d1721b616654e8bb172
Victor Mello <victorsmello93@gmail.com>
2021-05-12 21:36:12 -0300

    Microsserviço de pagamentos - Primeiro commit

---
+++ DevFreela.Payments/DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [Route("api/payments")]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PaymentInfoInputModel inputModel)
+        {
+            var result = await _paymentService.Process(inputModel);
+
+            if (!result)
+            {
+                return BadRequest("O pagamento não pôde ser realizado.");
+            }
+
+            return NoContent();
+        }
+    }
+}
